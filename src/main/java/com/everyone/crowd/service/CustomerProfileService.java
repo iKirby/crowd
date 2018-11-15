d7599f6d8b6a2e16c5f55e7ef5c2c5393cdc1bf2
iKirby <ikirby5556660@gmail.com>
2018-11-15 10:28:25 +0800

    Reformat code

--- src/main/java/com/everyone/crowd/service/CustomerProfileService.java
+++ src/main/java/com/everyone/crowd/service/CustomerProfileService.java
@@ -8,9 +8,14 @@ import org.springframework.stereotype.Service;
 
 public interface CustomerProfileService {
     CustomerProfile findById(Integer user_id);
+
     void insert(CustomerProfile customerProfile);
+
     void update(CustomerProfile customerProfile);
+
     void delete(Integer user_id);
-    void updateStatus (String status,Integer user_id);
-    void updateLevel (int level,Integer user_id);
+
+    void updateStatus(String status, Integer user_id);
+
+    void updateLevel(int level, Integer user_id);
 }
