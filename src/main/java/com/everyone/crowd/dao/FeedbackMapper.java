63b30351327795b806dc95872c0eead9e81f94f9
iKirby <ikirby5556660@gmail.com>
2018-11-16 09:50:44 +0800

    Implement feedback mapper and service

---
+++ src/main/java/com/everyone/crowd/dao/FeedbackMapper.java
@@ -0,0 +1,39 @@
+package com.everyone.crowd.dao;
+
+import com.everyone.crowd.entity.Feedback;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FeedbackMapper {
+
+    @Select("SELECT * FROM t_feedbacks WHERE id = #{id}")
+    Feedback findById(@Param("id") Integer id);
+
+    @Select("SELECT * FROM t_feedbacks LIMIT #{offset}, #{size}")
+    List<Feedback> findAll(@Param("offset") int offset, @Param("size") int size);
+
+    @Select("SELECT * FROM t_feedbacks WHERE reply IS NULL LIMIT #{offset}, #{size}")
+    List<Feedback> findNotReplied(@Param("offset") int offset, @Param("size") int size);
+
+    @Select("SELECT * FROM t_feedbacks WHERE user_id = #{userId} LIMIT #{offset}, #{size}")
+    List<Feedback> findByUserId(@Param("userId") Integer userId, @Param("offset") int offset, @Param("size") int size);
+
+    @Select("SELECT COUNT(id) FROM t_feedbacks")
+    int count();
+
+    @Select("SELECT COUNT(id) FROM t_feedbacks WHERE reply IS NULL")
+    int countNotReplied();
+
+    @Select("SELECT COUNT(id) FROM t_feedbacks WHERE user_id = #{userId}")
+    int countByUserId(Integer userId);
+
+    @Insert("INSERT INTO t_feedbacks (user_id, demand_id, title, content) VALUES (#{userId}, #{demandId}, #{title}, #{content})")
+    @Options(useGeneratedKeys = true)
+    int insert(Feedback feedback);
+
+    @Update("UPDATE t_feedbacks SET reply = #{reply} WHERE id = #{id}")
+    int reply(@Param("id") Integer id, @Param("reply") String reply);
+}
