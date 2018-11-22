69fa7196cfcdfaa9b2052cc93d3711ce8755c2b1
iKirby <ikirby5556660@gmail.com>
2018-11-22 16:06:46 +0800

    Remove unused tests

--- src/test/java/com/everyone/crowd/CrowdApplicationTests.java
+++ src/test/java/com/everyone/crowd/CrowdApplicationTests.java
@@ -1,15 +1,11 @@
 package com.everyone.crowd;
 
-import com.everyone.crowd.service.MailService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.sql.DataSource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,21 +16,5 @@ public class CrowdApplicationTests {
 
     @Test
     public void contextLoads() {
-    }
-
-    @Test
-    public void testDataSource() {
-        DataSource dataSource = context.getBean(DataSource.class);
-        System.out.println(dataSource.getClass().getName());
-
-        DataSourceProperties properties = context.getBean(DataSourceProperties.class);
-        System.out.println(properties.getUrl());
-    }
-
-    @Test
-    public void testMailSend() {
-        MailService mailService = context.getBean(MailService.class);
-
-        mailService.sendSimpleMessage("ikirby@qq.com", "测试邮件发送", "测试邮件内容");
     }
 }
