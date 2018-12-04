42b41fa6f00455843020ed9eb1fdcf459d48e6ac
iKirby <ikirby5556660@gmail.com>
2018-12-04 10:21:44 +0800

    Add file upload and related configuration

---
+++ src/main/java/com/everyone/crowd/controller/FileController.java
@@ -0,0 +1,33 @@
+package com.everyone.crowd.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class FileController {
+    @Value("${com.everyone.crowd.upload.path}")
+    private String uploadPath;
+
+    @GetMapping("/uploads/{fileName}")
+    public ResponseEntity<Resource> serveImageFile(@PathVariable String fileName) {
+        Resource file = new FileSystemResource(uploadPath + fileName);
+        if (file.exists() && file.isReadable()) {
+            ResponseEntity.BodyBuilder builder = ResponseEntity.ok();
+            if (fileName.endsWith(".png")) {
+                builder.contentType(MediaType.IMAGE_PNG);
+            } else if (fileName.endsWith(".jpg") || fileName.endsWith(".jpeg")){
+                builder.contentType(MediaType.IMAGE_JPEG);
+            } else {
+                builder.contentType(MediaType.APPLICATION_OCTET_STREAM);
+            }
+            return builder.body(file);
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
