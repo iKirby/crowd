69bc9b4c2edaf90b0f9b9d4c9b8343caeef1def0
iKirby <ikirby5556660@gmail.com>
2018-12-07 17:17:02 +0800

    Add actual memory value

--- src/main/java/com/everyone/crowd/controller/AdminController.java
+++ src/main/java/com/everyone/crowd/controller/AdminController.java
@@ -1,13 +1,18 @@
 package com.everyone.crowd.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
 
     @GetMapping("/admin/dashboard")
-    public String dashboard() {
+    public String dashboard(Model model) {
+        long totalMemory = Runtime.getRuntime().totalMemory();
+        long freeMemory = Runtime.getRuntime().freeMemory();
+        model.addAttribute("usedMemory", (totalMemory - freeMemory) / 1024 / 1024);
+        model.addAttribute("freeMemory", freeMemory / 1024 / 1024);
         return "admin/index";
     }
 }
