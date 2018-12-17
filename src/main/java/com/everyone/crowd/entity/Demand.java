40d6e990971cefd3a8c19651d74f507edc5158dc
iKirby <ikirby5556660@gmail.com>
2018-12-17 11:11:10 +0800

    Support Markdown in Demands' details

--- src/main/java/com/everyone/crowd/entity/Demand.java
+++ src/main/java/com/everyone/crowd/entity/Demand.java
@@ -3,6 +3,9 @@ package com.everyone.crowd.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigDecimal;
@@ -27,4 +30,11 @@ public class Demand {
     private String detail;
     private String attachment;
     private String status;
+
+    public String renderDetailHtml() {
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(detail);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(document);
+    }
 }
