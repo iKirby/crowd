f6a12e636f6230245cc2a58e6e69c0fa936ae89d
iKirby <ikirby5556660@gmail.com>
2019-01-09 18:12:54 +0800

    Do not allow deleting if verify request is not processed & refine related pages

--- src/main/java/com/everyone/crowd/controller/admin/VerifyRequestManageController.java
+++ src/main/java/com/everyone/crowd/controller/admin/VerifyRequestManageController.java
@@ -3,6 +3,7 @@ package com.everyone.crowd.controller.admin;
 import com.everyone.crowd.entity.Message;
 import com.everyone.crowd.entity.Page;
 import com.everyone.crowd.entity.VerifyRequest;
+import com.everyone.crowd.entity.exception.NotAcceptableException;
 import com.everyone.crowd.service.CustomerProfileService;
 import com.everyone.crowd.service.DevProfileService;
 import com.everyone.crowd.service.VerifyRequestService;
@@ -87,5 +88,18 @@ public class VerifyRequestManageController {
         }
 
         return "admin/verify-view";
+    }
+
+    @GetMapping("/admin/user/verify/delete/{id}")
+    public String deleteVerifyReq(HttpServletResponse response, @PathVariable("id") Integer id) {
+        VerifyRequest request = verifyRequestService.findById(id);
+        if (request.isProcessed()) {
+            CookieUtil.addMessage(response, "admin",
+                    new Message(Message.TYPE_SUCCESS, "认证请求已经删除"), "/admin");
+            verifyRequestService.delete(id);
+            return "redirect:/admin/user/verify?processed=true";
+        } else {
+            throw new NotAcceptableException("认证请求未处理，无法删除");
+        }
     }
 }
