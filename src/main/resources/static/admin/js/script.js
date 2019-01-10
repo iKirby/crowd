2dd3d66fac6033c7b7e1dc35e32b9d51015dbf02
iKirby <ikirby5556660@gmail.com>
2019-01-10 10:14:32 +0800

    Add ajax load for admin space

--- src/main/resources/static/admin/js/script.js
+++ src/main/resources/static/admin/js/script.js
@@ -1,17 +1,63 @@
 require(['jquery', 'js-cookie'], function ($, Cookies) {
     $(function () {
-        var prefix = "admin";
-        var msgBar = $("#message-bar");
-        if (msgBar.length > 0) {
-            var content = Cookies.get(prefix + "_messageContent");
-            var type = Cookies.get(prefix + "_messageType");
-            if (content !== undefined && type !== undefined) {
-                $("#message-content").text(content);
-                msgBar.addClass(type);
-                msgBar.collapse("show");
-            }
-        }
-        Cookies.remove(prefix + "_messageContent", { path: '/admin' });
-        Cookies.remove(prefix + "_messageType", { path: '/admin' });
+        Turbolinks.setProgressBarDelay(0);
+        document.addEventListener("turbolinks:load", function () {
+            bindForm($);
+            showMessageBar($, Cookies);
+        });
+        bindForm($);
+        showMessageBar($, Cookies);
     });
 });
+
+function showMessageBar($, Cookies) {
+    var prefix = "admin";
+    var msgBar = $("#message-bar");
+    if (msgBar.length > 0) {
+        var content = Cookies.get(prefix + "_messageContent");
+        var type = Cookies.get(prefix + "_messageType");
+        if (content !== undefined && type !== undefined) {
+            $("#message-content").text(content);
+            msgBar.addClass(type);
+            msgBar.collapse("show");
+        }
+    }
+    Cookies.remove(prefix + "_messageContent", {path: '/admin'});
+    Cookies.remove(prefix + "_messageType", {path: '/admin'});
+}
+
+function bindForm($) {
+    $("form").submit(function (e) {
+        e.preventDefault();
+        if ($(this).attr("method") === "post") {
+            Turbolinks.controller.adapter.progressBar.setValue(0);
+            Turbolinks.controller.adapter.progressBar.show();
+            if ($(this).attr("enctype") === "multipart/form-data") {
+                $.ajax({
+                    type: "POST",
+                    url: $(this).attr("action"),
+                    data: new FormData(this),
+                    success: function (data, textStatus, request) {
+                        Turbolinks.visit(request.getResponseHeader("Turbolinks-Location"));
+                    },
+                    cache: false,
+                    contentType: false,
+                    processData: false
+                });
+            } else {
+                $.ajax({
+                    type: "POST",
+                    url: $(this).attr("action"),
+                    data: $(this).serialize(),
+                    success: function (data, textStatus, request) {
+                        Turbolinks.visit(request.getResponseHeader("Turbolinks-Location"));
+                    }
+                });
+            }
+        } else {
+            var url = $(this).attr("action") + "?" + $(this).serialize();
+            Turbolinks.visit(url);
+        }
+    })
+}
+
